9f1d8e110f9fe92ce4cf04f80ebd08307695431d
Sajith Dilhan <sajithdilhan@gmail.com>
2022-02-02 18:22:41 +0800

    Add project files.

---
+++ IndexTask.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHandling
+{
+    class IndexTask
+    {
+        public event EventHandler<string> Indexing;
+        public event EventHandler<string> IndexingFinished;
+        public void IndexProducts()
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                OnIndexing(i.ToString());
+            }
+
+            OnIndexingFinished("9999");
+        }
+
+        public virtual void OnIndexing(string index)
+        {
+            Indexing?.Invoke(this, index);
+        }
+
+        public virtual void OnIndexingFinished(string lastIndex)
+        {
+            IndexingFinished?.Invoke(this, lastIndex);
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EventHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            IndexTask indexTask = new IndexTask();
+            
+            indexTask.Indexing += LogWriter;
+            indexTask.IndexingFinished += IndexingCompleted;
+            indexTask.IndexProducts();
+
+        }
+
+        public static void LogWriter(Object sender, string index)
+        {
+            Console.WriteLine(index);
+
+        }
+
+        public static void IndexingCompleted(object sender, string lastIndex)
+        {
+            Console.WriteLine("Indexing finished at: "+lastIndex);
+        }
+    }
+}
